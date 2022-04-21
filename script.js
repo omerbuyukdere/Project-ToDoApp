6ff3e4f8a661fa46044b313dbd5b03697388d63d
Omer <100731091+omerbuyukdere@users.noreply.github.com>
2022-04-21 16:13:22 +0300

    Add files via upload
    created part(delete-item)

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const form = document.querySelector("#addTaskForm");
 const input = document.querySelector("#txtTaskName");
 const btnDeleteAll = document.querySelector("#btnDeleteAll");
 const taskList = document.querySelector("#task-list");
+const btnPlus = document.querySelector("#btnAddNewTask");
 
 eventListeners();
 
 function eventListeners() {
+  // add item
   form.addEventListener("submit", addNewItem);
+  // delete an item
+  taskList.addEventListener("click", deleteItem);
+  btnDeleteAll.addEventListener("click", deleteAllItem);
+  // click event
+  btnPlus.addEventListener("click", addNewItem);
 }
-
+// add item
 function addNewItem(e) {
   if (input.value === "") {
     alert("please enter item");
@@ -32,4 +39,37 @@ function addNewItem(e) {
   input.value = "";
 
   e.preventDefault();
+}
+// add item btn
+
+// delete item
+
+function deleteItem(e) {
+  let areyousure = confirm("Are you sure ?");
+  if ((e.target.className = "fas fa-times")) {
+    areyousure;
+    if (areyousure === true) {
+      e.target.parentElement.parentElement.remove();
+    } else {
+      console.log(e.target);
+    }
+  }
+  e.preventDefault();
+}
+
+//delete all item
+function deleteAllItem(e) {
+  let areyousure = confirm("Are you sure ?");
+  if (
+    (e.target.className =
+      "btn btn-outline-danger btn-sm delete-all float-right")
+  ) {
+    areyousure;
+    if (areyousure === true) {
+      taskList.innerHTML = "";
+    }
+    if (areyousure === false) {
+      console.log(e.target);
+    }
+  }
 }
